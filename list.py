3e7591f26f3702ba9c40ed7049e34dc4e3ba464c
varshalearner <98809056+varshalearner@users.noreply.github.com>
2022-05-31 23:34:09 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+# Python list example
+list_example = ["apple", "banana", "cherry", "cherry"]
+print(list_example)
+
+# Length of list example
+name_list=['Mark','John','Karl']
+print("Name list length :"+str(len(name_list)))
+
+# Accessing the index at 0
+list_example = ["apple", "banana", "cherry"]
+print(list_example[0])
+
+# Adding items in list
+list_example.append("orange")
+
+#Replace items in list
+list_example.insert(1, "orange")
+
+#Print all the content of the list
+print(list_example)
+
+
+# Python print all content in list using for loop
+for x in list_example: print(x)
