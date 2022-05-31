a6823578ba7b3c0ab0f5b784d739af8d232e08df
varshalearner <98809056+varshalearner@users.noreply.github.com>
2022-05-31 07:40:42 -0700

    Create listappend.py

---
+++ listappend.py
@@ -0,0 +1,15 @@
+numbers = [10, 20, 30, 40, 50]
+
+print("Numeric List Before Appending Item")
+print(numbers)
+
+numbers.append(65)
+
+print("Numeric List After Appending First Item")
+print(numbers)
+
+value = int(input("Please enter the List Item = "))
+numbers.append(value)
+
+print("Numeric List After Appending Second Item")
+print(numbers)
