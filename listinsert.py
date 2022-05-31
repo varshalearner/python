12c5abef6e0d78058f88f497482f3d37d3fa7bee
varshalearner <98809056+varshalearner@users.noreply.github.com>
2022-05-31 07:41:21 -0700

    Create listinsert.py

---
+++ listinsert.py
@@ -0,0 +1,16 @@
+countries = ["India", "USA", "UK", "Italy"]
+
+print("List Before Appending Item")
+print(countries)
+
+countries.insert(3, "Japan")
+print("\nList After Appending Japan at 3rd Index Position")
+print(countries)
+
+countries.insert(0, "China")
+print("\nList After Appending China at 0 Index Position")
+print(countries)
+
+countries.insert(6, "France")
+print("\nList After Appending France at 6 Index Position")
+print(countries)
