46e5b9611e5874557763b8c75fcc9c8d1db28407
varshalearner <98809056+varshalearner@users.noreply.github.com>
2022-05-31 07:37:14 -0700

    Create prog10.py

---
+++ prog10.py
@@ -0,0 +1,19 @@
+#      *     *
+#     **    **
+#    ***   ***
+#   ****  ****
+#  ***** *****
+# ************
+r=int(input())
+for i in range (1,r+1):
+for j in range (r-i):
+print(" ",end="")
+for j in range (i):
+print("*"
+,end="")
+for j in range (r-i):
+print(" ",end="")
+for j in range (i):
+print("*"
+,end="")
+print()
